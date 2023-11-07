6c5a77eae29386578916392f9666a2e77ee7d2dc
Damián Ariel Betancur <da_ariel@hotmail.com>
2023-11-07 10:53:59 -0300

    login

--- src/main/java/org/itec/controller/LoginController.java
+++ src/main/java/org/itec/controller/LoginController.java
@@ -1,22 +1,222 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package org.itec.controller;
 
+import com.google.protobuf.TextFormat;
+import dao.Conexion;
+import dao.JornadaLaboralOperadorJpaController;
+import dao.MunicipalidadJpaController;
+import dao.TipoJornadaLaboralJpaController;
+import dao.UsuarioJpaController;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.JornadaLaboralOperador;
+import model.Municipalidad;
+import model.Usuario;
+
 /**
+ * Controlador de Login de Usuario
  *
- * @author Usuario
  */
 public class LoginController {
-    
-    public Boolean login(String user){
-        
-        if (user.equals("admin")) {
-            return true;
-        }
-        
-        return false;
+
+    //DAO
+    private final UsuarioJpaController usuarioDAO;
+    //Model
+    private static Usuario usuarioRegistradoInstanciaUnica = null;
+
+    public LoginController() {
+        //Inicializacion de DAO
+        this.usuarioDAO = new UsuarioJpaController(Conexion.getEmf());
     }
-    
+
+    /**
+     * Creacion Singleton Usuario Logeado
+     */
+    private synchronized static void createInstanceUsuario() {
+        if (usuarioRegistradoInstanciaUnica == null) {
+            usuarioRegistradoInstanciaUnica = new Usuario();
+        }
+    }
+
+    /**
+     * Devuele la instancia unica del usuario logeado
+     *
+     * @return
+     */
+    public static Usuario getInstanceUsuario() {
+        createInstanceUsuario();
+        return usuarioRegistradoInstanciaUnica;
+    }
+
+    /**
+     * Creacion Singleton organismo
+     */
+    private synchronized static void createInstanceOrganismo() {
+        if (organismoInstanciaUnica == null) {
+            organismoInstanciaUnica = new Municipalidad();
+        }
+    }
+
+    /**
+     * Devuele la instancia unica del Municipalidad
+     *
+     * @return
+     */
+    public static Municipalidad getInstanceMunicipalidad() {
+        createInstanceUsuario();
+        return organismoInstanciaUnica;
+    }
+
+    /**
+     * Recibe un usuario desde la vista Si el usuario y el password concuerdan,
+     * crea instancia unica de usuario proveniente de la base de datos y
+     * devuelve verdadero Si el usuario no concuerda devuelve falso
+     *
+     * @param unUsuario
+     * @return
+     */
+    public boolean iniciarSesion(Usuario unUsuario) {
+        boolean estado = false;
+        usuarioRegistradoInstanciaUnica = usuarioDAO.iniciarSesion(unUsuario);
+        if (usuarioRegistradoInstanciaUnica != null) {
+            try {
+                //Instacia Unica Singleton
+                createInstanceUsuario();
+                estado = true;
+
+                JornadaLaboralOperador jlop = jornadaLaboralOperadorDAO.buscarInicioJornadaLaboralOperador(usuarioRegistradoInstanciaUnica.getUnOperador().getUnDepartamentoA(), usuarioRegistradoInstanciaUnica.getUnOperador(), this.tipoJornadaLaboralDAO.findTipoJornadaLaboral(1l), Conexion.getFechaActual());
+
+                //Se verifica que es el primer inicio de sesion para ingresarlo al registro de jornada laboral
+                if (jlop == null) {
+                    System.out.println("Nueva Jornada Laboral");
+                    this.unaJornadaLabora = new JornadaLaboralOperador();
+
+                    this.unaJornadaLabora.setUnDepartamentoD(usuarioRegistradoInstanciaUnica.getUnOperador().getUnDepartamentoA());
+                    this.unaJornadaLabora.setUnOperador(usuarioRegistradoInstanciaUnica.getUnOperador());
+                    //Tipo de jornada laboral Login
+                    this.unaJornadaLabora.setUnTipoJornadaLaboral(this.tipoJornadaLaboralDAO.findTipoJornadaLaboral(1l));
+                    this.unaJornadaLabora.setUnaFecha(Conexion.getFechaActual());
+                    this.unaJornadaLabora.setUnaHora(Conexion.getFechaActual());
+
+                    this.jornadaLaboralOperadorDAO.create(unaJornadaLabora);
+                } else {
+                    System.out.println("Ya inicio Jornada Laboral");
+
+                    jlop = jornadaLaboralOperadorDAO.buscarInicioJornadaLaboralOperador(usuarioRegistradoInstanciaUnica.getUnOperador().getUnDepartamentoA(), usuarioRegistradoInstanciaUnica.getUnOperador(), this.tipoJornadaLaboralDAO.findTipoJornadaLaboral(4l), Conexion.getFechaActual());
+                    if (jlop != null) {
+                        //Volvio de almorzar
+                        jlop = jornadaLaboralOperadorDAO.buscarInicioJornadaLaboralOperador(usuarioRegistradoInstanciaUnica.getUnOperador().getUnDepartamentoA(), usuarioRegistradoInstanciaUnica.getUnOperador(), this.tipoJornadaLaboralDAO.findTipoJornadaLaboral(3l), Conexion.getFechaActual());
+                        if (jlop != null) {
+
+                            System.out.println("Continua Jornada Laboral volvio de almorzar");
+                            this.unaJornadaLabora = new JornadaLaboralOperador();
+
+                            this.unaJornadaLabora.setUnDepartamentoD(usuarioRegistradoInstanciaUnica.getUnOperador().getUnDepartamentoA());
+                            this.unaJornadaLabora.setUnOperador(usuarioRegistradoInstanciaUnica.getUnOperador());
+                            //Tipo de jornada laboral Login
+                            this.unaJornadaLabora.setUnTipoJornadaLaboral(this.tipoJornadaLaboralDAO.findTipoJornadaLaboral(4l));
+                            this.unaJornadaLabora.setUnaFecha(Conexion.getFechaActual());
+                            this.unaJornadaLabora.setUnaHora(Conexion.getFechaActual());
+
+                            this.jornadaLaboralOperadorDAO.create(unaJornadaLabora);
+                        } else {
+                            System.out.println("Ya comenzo a trabajar y ya almorzo");
+                        }
+                    }else{
+                        System.out.println("ya se deslogeo no puede cargar mas registros en el dia");
+                    }
+
+                }
+            } catch (TextFormat.ParseException ex) {
+                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ParseException ex) {
+                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return estado;
+    }
+
+    public boolean almorzar() {
+        boolean estado = false;
+        if (usuarioRegistradoInstanciaUnica != null) {
+            try {
+
+                JornadaLaboralOperador jlop = jornadaLaboralOperadorDAO.buscarInicioJornadaLaboralOperador(usuarioRegistradoInstanciaUnica.getUnOperador().getUnDepartamentoA(), usuarioRegistradoInstanciaUnica.getUnOperador(), this.tipoJornadaLaboralDAO.findTipoJornadaLaboral(4l), Conexion.getFechaActual());
+
+                if (jlop == null) {
+                    jlop = jornadaLaboralOperadorDAO.buscarInicioJornadaLaboralOperador(usuarioRegistradoInstanciaUnica.getUnOperador().getUnDepartamentoA(), usuarioRegistradoInstanciaUnica.getUnOperador(), this.tipoJornadaLaboralDAO.findTipoJornadaLaboral(3l), Conexion.getFechaActual());
+
+                    //Se verifica que es el primer inicio de sesion para ingresarlo al registro de jornada laboral
+                    if (jlop == null) {
+
+                        System.out.println("Nuevo Almuezo");
+                        this.unaJornadaLabora = new JornadaLaboralOperador();
+
+                        this.unaJornadaLabora.setUnDepartamentoD(usuarioRegistradoInstanciaUnica.getUnOperador().getUnDepartamentoA());
+                        this.unaJornadaLabora.setUnOperador(usuarioRegistradoInstanciaUnica.getUnOperador());
+                        //Tipo de jornada laboral Login
+                        this.unaJornadaLabora.setUnTipoJornadaLaboral(this.tipoJornadaLaboralDAO.findTipoJornadaLaboral(3l));
+                        this.unaJornadaLabora.setUnaFecha(Conexion.getFechaActual());
+                        this.unaJornadaLabora.setUnaHora(Conexion.getFechaActual());
+
+                        this.jornadaLaboralOperadorDAO.create(unaJornadaLabora);
+                        estado = true;
+                    } else {
+                        System.out.println("Ya almorzo");
+
+                    }
+                } else {
+                    System.out.println("ya se deslogeo no puede cargar mas registros en el dia");
+                }
+
+            } catch (TextFormat.ParseException ex) {
+                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ParseException ex) {
+                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return estado;
+    }
+
+    public boolean finalizarJornadaLaboral() {
+        boolean estado = false;
+        if (usuarioRegistradoInstanciaUnica != null) {
+            try {
+
+                JornadaLaboralOperador jlop = jornadaLaboralOperadorDAO.buscarInicioJornadaLaboralOperador(usuarioRegistradoInstanciaUnica.getUnOperador().getUnDepartamentoA(), usuarioRegistradoInstanciaUnica.getUnOperador(), this.tipoJornadaLaboralDAO.findTipoJornadaLaboral(2l), Conexion.getFechaActual());
+
+                //Se verifica que es el primer inicio de sesion para ingresarlo al registro de jornada laboral
+                if (jlop == null) {
+
+                    System.out.println("Nuevo Finalizacion de Jornada Laboral");
+                    this.unaJornadaLabora = new JornadaLaboralOperador();
+
+                    this.unaJornadaLabora.setUnDepartamentoD(usuarioRegistradoInstanciaUnica.getUnOperador().getUnDepartamentoA());
+                    this.unaJornadaLabora.setUnOperador(usuarioRegistradoInstanciaUnica.getUnOperador());
+                    //Tipo de jornada laboral Login
+                    this.unaJornadaLabora.setUnTipoJornadaLaboral(this.tipoJornadaLaboralDAO.findTipoJornadaLaboral(2l));
+                    this.unaJornadaLabora.setUnaFecha(Conexion.getFechaActual());
+                    this.unaJornadaLabora.setUnaHora(Conexion.getFechaActual());
+
+                    this.jornadaLaboralOperadorDAO.create(unaJornadaLabora);
+                    estado = true;
+                } else {
+                    System.out.println("Ya Finalizo la jornada Laboral");
+
+                }
+            } catch (TextFormat.ParseException ex) {
+                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ParseException ex) {
+                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return estado;
+    }
+
 }
---
+++ src/main/java/org/itec/model/Usuario.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.itec.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Ariel
+ */
+@Entity
+@Table(name = "usuario")
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "clave")
+    private String clave;
+
+    @ManyToOne
+    @JoinColumn(name = "id_tipoUsuario")
+    private TipoUsuario tipoUsuario;
+    
+   
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Usuario)) {
+            return false;
+        }
+        Usuario other = (Usuario) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.Usuario[ id=" + id + " ]";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public TipoUsuario getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(TipoUsuario tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public Persona getUnaPersona() {
+        return unaPersona;
+    }
+
+    public void setUnaPersona(Persona unaPersona) {
+        this.unaPersona = unaPersona;
+    }
+
+    public Operador getUnOperador() {
+        return unOperador;
+    }
+
+    public void setUnOperador(Operador unOperador) {
+        this.unOperador = unOperador;
+    }
+
+    
+
+    
+
+}
